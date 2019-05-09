c2e3b1228df5abd480162b05c85ef63a345fc3c3
LeonLuna98 <conradolunap@gmail.com>
2019-05-09 19:53:12 -0300

    Ejercicio 1 y 2

---
+++ tp6.pto1/tp6.pto1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp6.pto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+   
+            int aux=0;
+            int aux2 = 0;
+            if (N > 0)
+            {
+                while (N != 0) { 
+                aux2 = N % 10;
+                aux = aux * 10 + aux2;
+                N = N / 10;
+               
+                }
+                Console.WriteLine("Numero invertido:" + aux);
+            }
+            else
+            {
+
+                Console.WriteLine(0);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tp6.pto1/tp6.pto2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp6.pto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char operador;
+            int a, b,c;
+            Console.Write("Ingrese un operador + , - , * , /: ");
+            operador = char.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el primer numero: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero: ");
+            b =int.Parse(Console.ReadLine());
+                switch (operador){
+                    case '*':
+                        c = a * b;
+                        Console.WriteLine("La multiplicacion de " + a + " por " + b + " es " + c);
+                        break;
+                    
+                    case '-':                
+                        c = a - b;
+                        Console.WriteLine("La resta de " + a + " menos " + b + " es " + c);
+                        break;
+                    
+                    case '+':
+                        c = a + b;
+                        Console.WriteLine("La suma de " + a + " mas " + b + " es " + c);
+                        break;
+                    
+                    case '/':
+                        c = a / b;
+                        Console.WriteLine("La division entera de " + a + " sobre " + b + " es " + c);
+                        break;
+                    
+                }
+            Console.ReadLine();
+            
+        }
+    }
+}
